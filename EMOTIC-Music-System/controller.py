565528dd6fd63b1c48493702cac993c19fd436cb
Ana Zeneli <anazeneli@Anas-MacBook-Pro.local>
2019-04-13 14:18:41 -0400

    Faster access to bounding box coordinates. Created cropped images array of bodies for person feature extractor

--- EMOTIC-Music-System/controller.py
+++ EMOTIC-Music-System/controller.py
@@ -15,33 +15,53 @@ test_image = "EMOTIC/emotic/emodb_small/images/0n5sa6o2zfrg3shtoo.jpg"
 # person: body_bbox, annotations_categories, annotations_continuous, gender, age
 emotic = scipy.io.loadmat('EMOTIC/annotations/Annotations.mat')
 
-# create a dictionary of annotations structures 
-# hashed by fiale name 
-# length of value is number of persons annotated 
+# create a dictionary of annotations structures
+# hashed by file name
+# length of value is number of persons annotated
 # [['person'],['gender'],['annotations_categories'],['annotations_continuous'],['gender'],['age']]
 emotic_annotations = {}
+test_image = "mscoco/images/COCO_train2014_000000140322.jpg"
 
-print ("Creating body boxes")
-for i in range(len(emotic['train']['filename'][0])): 
+print("Creating body boxes")
+for i in range(len(emotic['train']['filename'][0])):
     file = emotic['train']['filename'][0][i][0]
-    
-    # if more than one person is the focus of the image 
-    for j in range(len(emotic['train']['person'][0][i][0])):      
-        if file in emotic_annotations: 
-            emotic_annotations[file].append([np.array(emotic['train']['person'][0][i][0][j]['body_bbox'][0]).tolist(),
-                                             np.array(np.array(np.array(emotic['train']['person'][0][i][0][j]['annotations_categories']).tolist()).tolist()).flatten().tolist(),      
-                                             np.array(np.array(emotic['train']['person'][0][i][0][j]['annotations_continuous']).tolist()).flatten().tolist(),
-                                             np.array(emotic['train']['person'][0][i][0][j]['gender']).tolist(),
-                                             np.array(emotic['train']['person'][0][i][0][j]['age']).tolist()])            
-        else: 
-            emotic_annotations[file]= [[np.array(emotic['train']['person'][0][i][0][j]['body_bbox'][0]).tolist(),
-                                        np.array(np.array(np.array(emotic['train']['person'][0][i][0][j]['annotations_categories']).tolist()).tolist()).flatten().tolist(),                             
-                                        np.array(np.array(emotic['train']['person'][0][i][0][j]['annotations_continuous']).tolist()).flatten().tolist(),
-                                        np.array(emotic['train']['person'][0][i][0][j]['gender']).tolist(),
-                                        np.array(emotic['train']['person'][0][i][0][j]['age']).tolist()]]    
+    # if more than one person is the focus of the image
+    for j in range(len(emotic['train']['person'][0][i][0])):
+        file_path = str(emotic['train']['folder'][0][i][0]) + "/" + file
 
-# for k,v in emotic_annotations.items(): 
-#     print(k, v)
+        #         if file_path == test_image:
+        #             print(np.array(emotic['train']['person'][0][i][0][j]['body_bbox'][0]).tolist())
+        #             print(np.array(np.array(np.array(emotic['train']['person'][0][i][0][j]['annotations_categories']).tolist()).tolist()).flatten().tolist())
+        #             print(np.array(np.array(emotic['train']['person'][0][i][0][j]['annotations_continuous']).tolist()).flatten().tolist())
+        #             print(np.array(emotic['train']['person'][0][i][0][j]['gender']).tolist())
+        #             print(np.array(emotic['train']['person'][0][i][0][j]['age']).tolist())
+
+        if file_path in emotic_annotations:
+            emotic_annotations[file_path] = np.append(emotic_annotations[file_path],
+                                                      # tuples for bounding box coordinates for Image.crop
+                                                      [[tuple(np.array(
+                                                          emotic['train']['person'][0][i][0][j]['body_bbox'][
+                                                              0]).tolist()),
+                                                        np.array(np.array(np.array(
+                                                            emotic['train']['person'][0][i][0][j][
+                                                                'annotations_categories']).tolist()).tolist()).flatten().tolist(),
+                                                        np.array(np.array(emotic['train']['person'][0][i][0][j][
+                                                                              'annotations_continuous']).tolist()).flatten().tolist(),
+                                                        np.array(
+                                                            emotic['train']['person'][0][i][0][j]['gender']).tolist(),
+                                                        np.array(
+                                                            emotic['train']['person'][0][i][0][j]['age']).tolist()]],
+                                                      axis=0)
+
+        else:
+            emotic_annotations[file_path] = np.array(
+                [[tuple(np.array(emotic['train']['person'][0][i][0][j]['body_bbox'][0]).tolist()),
+                  np.array(np.array(np.array(emotic['train']['person'][0][i][0][j][
+                                                 'annotations_categories']).tolist()).tolist()).flatten().tolist(),
+                  np.array(np.array(
+                      emotic['train']['person'][0][i][0][j]['annotations_continuous']).tolist()).flatten().tolist(),
+                  np.array(emotic['train']['person'][0][i][0][j]['gender']).tolist(),
+                  np.array(emotic['train']['person'][0][i][0][j]['age']).tolist()]])
     
 # pass bounding box parameters to person to handle
 person_features = PersonFeatures(emotic_annotations)
--- EMOTIC-Music-System/person_features.py
+++ EMOTIC-Music-System/person_features.py
@@ -2,18 +2,56 @@ from keras_vggface.vggface import VGGFace
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 from keras.utils.data_utils import get_file
+from PIL import Image
 import numpy as np
+import scipy.io
+
 
 class PersonFeatures():
-    def __init__(self, bodies):
-        self.bodies = bodies
-        # TODO: Look into annotation of bounding boxes
+    def __init__(self, annotations):
+        # pull out body boxes
+        self.bodies = annotations
 
-        # TODO: Use bounding box coordinates as dataset for person features
-        #  Look into transfer learning/fine-tuning for ImageNet
-        #  create 16 layer convolutional neural network with  d1-D kernel
-        #  output features of final layer
+        # crop images
+        cropped_images = []
+        # load in training images body boxes
+        for k, v in self.bodies.items():
+            for b in v[:, 0]:
+                # open image for cropping
+                img = Image.open("EMOTIC/emotic/" + test_image)
+                #         img.show()
+                # crop the image
+                img_crop = img.crop(box=b)
+                #         img_crop.show()
+                cropped_images.append(b)
 
-    def vgg_model():
+    #         self.features = vgg_model(cropped_images)
+
+    def vgg_model(images):
         face_model = VGGFace(model='resnet50', include_top=False)
 
+        # preprocess images
+        # TODO: preprocess images
+        features = []
+        for i in images:
+            # load image setting the image size to 224 x 224
+            img = image.load_img(img_path, target_size=(224, 224))
+
+            # convert image to numpy array
+            x = image.img_to_array(img)
+            # the image is now in an array of shape (3, 224, 224)
+            # need to expand it to (1, 3, 224, 224) as preprocess takes in a list
+            x = np.expand_dims(x, axis=0)
+            x = preprocess_input(x)
+            # extract the features
+            feats = model.predict(x)[0]
+            # convert from Numpy to a list of values
+            features.append(np.char.mod('%f', feats))
+
+            # TODO: reduce feature dimensionality
+
+        return features
+
+
+# p = PersonFeatures(emotic_annotations)
+# print (p.features)
